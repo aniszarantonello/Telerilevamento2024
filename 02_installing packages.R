f87fd99fd8d1372e045aea6b13dccb3218b7bc53
aniszarantonello <162352330+aniszarantonello@users.noreply.github.com>
2024-05-10 16:50:50 +0200

    Update 02_installing packages.R

--- 02_installing packages.R
+++ 02_installing packages.R
@@ -1,4 +1,7 @@
 #installing new packages in R
-
 install.packages("terra") #with this function we can install packages from the CRAN 
 library(terra) #you can call the package and hope there aren't any problems
+install.packages("devtools") #we will use it later
+library(devtools)
+install_github("ducciorocchini/imageRy") #this is how we can install packages from github
+library(imageRy) 
