ccd4d54ec0522fa742c9b5a9f9dc17addc54874e
aniszarantonello <162352330+aniszarantonello@users.noreply.github.com>
2024-05-15 15:21:57 +0200

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -1,27 +1,29 @@
-#indici di vegetazione
+#vegetation indeces
 
-library(terra) #come al solito i pacchetti vanno richiamati perchè ci servono per usare funzioni che R normalmente non ha
+library(terra) #as always we have to call back the packages because we need them to use some functions that R normally doesn't have
 library(imageRy)
 
-im.list() #ci viene data una lista dei file disponibili dal pacchetto
-#oggi usiamo le immagini del matogrosso del 1992
-im.import("matogrosso_l5_1992219_lrg.jpg") #importiamo l'immagine; dentro imagerY abbiamo le immagini con "" quidi vanno usate
-m1992<-im.import("matogrosso_l5_1992219_lrg.jpg") #così rinominiamo l'immagine
+im.list() #we ask for a list of all available files in the package 
 
+#today we use images from Matogrosso 1992
+im.import("matogrosso_l5_1992219_lrg.jpg") #we import the image; we have to use "" because in imageRy they are nominated in this way
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg") #we mention the image as we want
 
+# band 1 = nir = R
+# band 2 = red = G
+# band 3 = green = B
 
-im.plotRGB(m1992,1,2,3) #viene fuori la stessa immagine che plottava nel momento in cui l'abbiamo importata
-im.plotRGB(m1992,2,1,3) #così mettiamo l'infrarosso nella componente verde dell'immagine
+im.plotRGB(m1992, 1, 2, 3) #this is the same image we saw when we imported it 
+im.plotRGB(m1992, 2, 1, 3) #in this way we put the nir in the green component of the image 
+im.plotRGB(m1992, 2, 3, 1) #nir is on blue
 
-im.plotRGB(m1992,2,3,1) #cambiamo il nir sul blu
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg") #we import image from 2006 and we mention it directly. The automatic plot uses the first 3 bands 
+im.plotRGB(m2006, 1, 2, 3) #così plottiamo l'infrarosso nel rosso? il prof non lo fa perchè dice che è uguale.
+im.plotRGB(m2006, 2, 1, 3) #nir on green
+im.plotRGB(m2006, 2, 3, 1) #nir on blue
 
-m2006<-im.import("matogrosso_ast_2006209_lrg.jpg") #importiamo l'immagine del 2006 e le diamo un nuovo nome. il plot automatio usa le prime tre bande
-im.plotRGB(m2006,1,2,3) #così plottiamo l'infrarosso nel rosso? il prof non lo fa perchè dice che è uguale.
-im.plotRGB(m2006,2,1,3) #nir nel verde
-im.plotRGB(m2006,2,3,1) #nir nel blu
-
-#facciamo un  multiframe con tutte le immagini
-par(mfrow=c(2,3)) #due righe e tre colonne dove le righe sono 1992 e 2006 e le colonne il nir nelle diverse bande
+#we make a multiframe with all the images
+par(mfrow=c(2,3)) #2 rows and 3 columns where rows refers to 1992 and 2006, and in columns there is the nir in the various bands
 #adesso carichiamo tutte le immagini
 im.plotRGB(m1992,1,2,3) #nir on red
 im.plotRGB(m1992,2,1,3) #nir on green
