7a6f0bf01f79b84e9979e78e5b21357e2820d17a
aniszarantonello <162352330+aniszarantonello@users.noreply.github.com>
2024-05-24 11:39:27 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate analysis.R
+++ 09_multivariate analysis.R
@@ -15,74 +15,23 @@ sentdo <- c(sent2, sent3, sent4, sent8)
 sentdo
 
 #visualizing the image
-im.plotRGB(sentdo, r=4,g=3,b=2) #metto il nir nel rosso (vegetazione viene rossa), la componente rossa è visualizzata nel verde e la componente verde è visualizzata nel blu. 
-# posso anche visualizzarlo con il nir nel verde 
-im.plotRGB(sentdo, r=3,g=4,b=2)
+im.plotRGB(sentdo, r=4, g=3, b=2) #nir is on red, the red component will be visualized in green and green in blue  
+im.plotRGB(sentdo, r=3, g=4, b=2) #nir on green 
 
-#calcola tutte le correlazioni tra le bande 
-dev.off() #sempre utile fare un po' di pulizia 
-pairs(sentdo) #attenzione, correlazione non è causalità. 
+dev.off()  
 
-#calcoliamo la pca 
-pcimage <- im.pca(sentdo)
-# restituisce i range delle varie componenti, la prima è quella a variabilità maggiore (la principale). Numericamente vengono un po' diversi se faccio diverse ripetizioni, questo è dovuto al fatto che si basa su un campione estratto casualmente 
-# sotto c'è anche la correlazione tra banda e componenti estratte dall'analisi. 
-
-# sommiamo i vari range di tutti gli assi ottenuti per avere la variabilità totale
-tot <- sum(1705.70050, 570.32676, 43.70992, 25.22304)
-
-# calcolo la percentuale di variabilità che è spiegata da una certa componente 
-1705.70050 * 100 /tot #restituisce la percentuale di variabilità spiegata dalla prima componente 
-570.32676 * 100 / tot #percentuale dalla seconda 
-
-# creo una colorRampPalette
-vir <- colorRampPalette(viridis(7))(100) 
-plot(pcimage, col=vir) #vedo che la prima compontente è molto ben rappresentativa delle bande (immagine ancora si capisce), mentre nell'ultima non si capisce più nulla (immagine non chiara), solo rumore di fondo
-
-# vedo che posso anche scrivere direttamente plot(pcimage, col=viridis(100)) ed evitare di fare la colorampalette 
-plot(pcimage, col=viridis(100))
-# posso anche cambiare il colore 
-plot(pcimage, col=plasma(100))
-
-
-
----
-
-
-im.pca  #compattiamo il set in più dimensioni
-
-library(terra)
-library(imageRy)
-install.packages("viridis")
-library(viridis)
-
-im.list()
-#importing data
-b2<-im.import("sentinel.dolomites.b2.tif") #blue
-b3<-im.import("sentinel.dolomites.b3.tif") #green
-b4<-im.import("sentinel.dolomites.b4.tif") #red
-b8<-im.import("sentinel.dolomites.b8.tif") #nir
-
-#creating a stack
-sentdo<-c(b2,b3,b4,b8)
-
-im.plotRGB(sentdo, r=4, g=3, b=2)
-im.plotRGB(sentdo, r=3, g=4, b=2)
-
+#calculating bands correlations 
 pairs(sentdo)
 
-pcimage<-im.pca(sentdo) 
+#calcolculating pca 
+pcimage <- im.pca(sentdo) #the function give back the ranges of various components, the first one is the one with most of the variability (the principal). Speaking about numbers they will be a bit different every time due to a casual sample #below there is the correlation between the band and components extracted from the analysis
+tot <- sum(1705.70050, 570.32676, 43.70992, 25.22304) #we sum all various ranges of all axis obtained to have the total variability 
 
-#vogliamo estrarre le percentuali di ogni componente
-tot<-sum(1375.29098,639.56879,54.88589,38.57959) #la somma di tutti i vari range per ogni asse
-1375.29098*100/tot #per sapere la variabilità spiegata dalla prima componente; ha la maggiore varianza spiegata 
-639.56879*100/tot #la seconda componente ha il 30% #e così via per le altre, sempre a diminuire
+#Calculating the percentage of variability explained by a certain component
+570.32676 * 100 / tot #percentage of the second 
 
-vir<-colorRampPalette(viridis(7))(100)
-plot(pcimage, col=vir)
+vir <- colorRampPalette(viridis(7))(100) # creating a new color Palette
+plot(pcimage, col = vir) #the first component is very well representative of the bands, while in the last one we don't understand nothing from the image, only background noise
 
-#oppure
-plot(pcimage, col=viridis(100))
-
-#da un sistema quadridimensionale siamo riusciti a passare a un sistema quasi monodimensionale dove il 70% è spiegato dalla prima componente principale
-
+plot(pcimage, col = viridis(100)) #I can avoid to do the color palette in this way. Because they already exist in viridis
+plot(pcimage, col = plasma(100)) #changing color
