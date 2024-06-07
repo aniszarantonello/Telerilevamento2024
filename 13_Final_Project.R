270e0547437578b0878a733ecdc341bda4b8808b
aniszarantonello <162352330+aniszarantonello@users.noreply.github.com>
2024-06-07 16:57:27 +0200

    Update 13_Final_Project.R

--- 13_Final_Project.R
+++ 13_Final_Project.R
@@ -1,4 +1,4 @@
-## The aim of the project is to calculate the area damaged by the fire that affected Tenerife island from the 15th till the 30th of august 2023.
+## The aim of the project is calculate the area damaged by the fire that affected Tenerife island from the 15th till the 30th of august 2023.
 # Data taken from Copernicus Browser
 
 #first of all we recall all the packages we need 
@@ -16,12 +16,19 @@ setwd("C:/Users/anisz/Downloads") #we explain to the system in which folder of m
 
 cl.tn <- colorRampPalette(c("yellow", "slateblue", "wheat4")) (100)
 
-## july NBR
+#loading images
 july <- rast("luglio_swir.jpg")
+sept <- rast("settembre_swir.jpg")
 # 1 = B12 = SWIR
 # 2 = B8 = NIR
 # 3 = B4 = red
+par(mfrow = c(1,2)) 
+plot(july)
+plot(sept)
 
+dev.off()
+
+## july NBR
 plot(july[[1]], col = cl.tn)  #swir: absorbed by vegetation
 plot(july[[2]], col= cl.tn)  #nir: reflected by vegetation
 
@@ -35,7 +42,6 @@ viridis <- colorRampPalette(viridis(7))(255) #recall package viridis
 plot(NBR_july, col = viridis) # we use viridis to enhance differences
 
 ## september NBR
-sept <- rast("settembre_swir.jpg")
 plot(sept[[1]], col = cl.tn) #swir
 plot(sept[[2]], col= cl.tn) #nir
 diff.sept = sept[[2]] - sept[[1]]
@@ -48,178 +54,148 @@ plot(NBR_sept, col = viridis)
 
 #we put them on a stack
 NBRstack <- c(NBR_july, NBR_sept)
-names(NBR_stack) <- c("NBR july", "NBR september")
-plot(NBR_stack, col = viridis)
-#non funziona
+names(NBRstack) <- c("NBR july", "NBR september")
+plot(NBRstack, col = viridis)
 
 #dNBR or delta NBR can be used to estimate the burn severity. is the difference between the pre-fire and post-fire NBR 
 dNBR = (NBR_july) - (NBR_sept)
-plot(dNBRtn, col = viridis)
-#non funziona
+plot(dNBR, col = viridis)
 
+########
+#Now we zoom to the region that has been more damaged from the wildfire, we take a square of 13x13km
+#we calculate the NBR
+#we do the classification
 
+julyB <- rast("zona1_luglio_swir.jpg")
+plot(julyB)
+septB <- rast("zona1_sett_swir.jpg")
+plot(septB)
 
+par(mfrow = c(1,2)) 
+plot(julyB)
+plot(septB)
 
-#####da rivedere cosa classificare
-# Now we classify the land cover to see the differences between July and September
-#importing false color images:
-july2 <- rast("luglio_falsecolor.jpg")
-sept2 <- rast("settembre_falsecolor.jpg")
-# band 1 = nir = R
-# band 2 = red = G
-# band 3 = green = B
+dev.off()
 
-#classification: #quantiffying fire cover 
-julyc <- im.classify(july2, num_cluster = 3)
-#class 1 = forest #questi cambiano ogni volta quindi da rivedere 
-#class 2 = human and fire modification
-#class 3 = sea and desert
-septc <- im.classify(sept2, num_cluster = 3)
-#class 1 = forest
-#class 2 = human and fire modification
-#class 3 = sea and desert
+## julyB NBR
+plot(julyB[[1]], col = cl.tn)  #swir: absorbed by vegetation
+plot(julyB[[2]], col= cl.tn)  #nir: reflected by vegetation
 
-##potrei anche calcolare la variability
+diff.julyB = julyB[[2]] - julyB[[1]] 
+plot(diff.julyB, col = cl.tn)
+sum.julyB = julyB[[1]] + julyB[[2]]
+plot(sum.julyB, col = cl.tn)
+NBR_julyB = (diff.julyB) / (sum.julyB)
 
+viridis <- colorRampPalette(viridis(7))(255) #recall package viridis
+plot(NBR_julyB, col = viridis) # we use viridis to enhance differences
 
+## septemberB NBR
+plot(septB[[1]], col = cl.tn) #swir
+plot(septB[[2]], col= cl.tn) #nir
+diff.septB = septB[[2]] - septB[[1]]
+plot(diff.septB, col = cl.tn)
+sum.septB = septB[[1]] + septB[[2]]
+plot(sum.septB, col = cl.tn)
+NBR_septB = (diff.septB) / (sum.septB)
 
-#########################
+plot(NBR_septB, col = viridis)
 
+NBRstack2 <- c(NBR_julyB, NBR_septB)
+names(NBRstack2) <- c("NBR july", "NBR september")
+plot(NBRstack2, col = viridis)
 
+#dNBR or delta NBR can be used to estimate the burn severity. is the difference between the pre-fire and post-fire NBR 
+dNBR2 = (NBR_julyB) - (NBR_septB)
+plot(dNBR2, col = viridis)
+#A higher value of dNBR indicates more severe damage;
+#Areas with negative dNBR values may indicate regrowth following a fire.
 
-
-#importing images
-t07 <- rast("Tenerife_luglio.jpg") #this function creates SpatRaster objects; we load in R the image of Tenerife before the fire
-t07 #asking for the info about the images: the bands ecc
-plot(t07) #to see the image
-t09 <- rast("Tenerife_settembre.jpg") #loading the image of the island after one month of fire
-t09 #asking for the info about the images: the bands ecc
-
-#multiframe of natural colors to see the differences
-par(mfrow=c(1,2))
-plot(t07)
-plot(t09)
-
-#fin qua allenamento ma non so quanto senso abbia
-
-#importing data: single bands
-setwd("C:/Users/anisz/Downloads") #we explain to the system in which folder of my PC it has to extract the data we want to import 
-
-B02luglio <- rast("B02_luglio.jpg") #--> blue
-B03luglio <- rast("B03_luglio.jpg") #--> green
-B04luglio <- rast("B04_luglio.jpg") #--> red
-B08luglio <- rast("B08_luglio.jpg") #--> nir
-B02settembre <- rast("B02_settembre.jpg") #--> blue
-B03settembre <- rast("B03_settembre.jpg") #--> green
-B04settembre <- rast("B04_settembre.jpg") #--> red
-B08settembre <- rast("B08_settembre.jpg") #--> nir
-
-#stacksent luglio
-vir <- colorRampPalette(viridis(7)) (256)
-stacksent <- c(B02luglio, B03luglio, B04luglio, B08luglio) 
-plot(stacksent, col = vir)
-
-#RGB plotting
-#stacksent[[1]] = b2 = blue
-#stacksent[[2]] = b3 = green
-#stacksent[[3]] = b4 = red
-#stacksent[[4]] = b8 = nir 
-par(mfrow = c(2,2)) #let's create a multiframe to see nir in every band
-im.plotRGB(stacksent, 3, 2, 1) #natural color
-im.plotRGB(stacksent, 4, 3, 2) #nir on red
-im.plotRGB(stacksent, 3, 4, 2) #nir on green
-im.plotRGB(stacksent, 3, 2, 4) #nir on blue
-
-#stacksent settembre
-vir <- colorRampPalette(viridis(7)) (256)
-stacksent2 <- c(B02settembre, B03settembre, B04settembre, B08settembre) 
-plot(stacksent2, col = vir)
-
-#RGB plotting
-#stacksent2[[1]] = b2 = blue
-#stacksent2[[2]] = b3 = green
-#stacksent2[[3]] = b4 = red
-#stacksent2[[4]] = b8 = nir 
-par(mfrow = c(2,2))
-im.plotRGB(stacksent2, 3, 2, 1) #natural color
-im.plotRGB(stacksent2, 4, 3, 2) #nir on red
-im.plotRGB(stacksent2, 3, 4, 2) #nir on green
-im.plotRGB(stacksent2, 3, 2, 4) #nir on blue
-
-#differenza luglio-settembre
-par(mfrow = c(1,2))
-plot(B08luglio, col = vir)
-plot(B08settembre, col = vir)
-
-#confronto tra nir
-par(mfrow = c(1,2))
-im.plotRGB(stacksent, 4, 3, 2) #nir on red in july
-im.plotRGB(stacksent2, 4, 3, 2) #nir on red in september #we see a clearly difference
-
-
-#importing false color images:
-luglio <- rast("luglio_falsecolor.jpg")
-settembre <- rast("settembre_falsecolor.jpg")
-
-# band 1 = nir = R
-# band 2 = red = G
-# band 3 = green = B
-
-par(mfrow = c(2,3)) #2 rows and 3 columns where rows refers to july and september, and in columns there is the nir in the various bands
-#now we load all images 
-im.plotRGB(luglio, 1, 2, 3) #nir on red
-im.plotRGB(luglio, 2, 1, 3) #nir on green
-im.plotRGB(luglio, 2, 3, 1) #nir on blue
-im.plotRGB(settembre, 1, 2, 3) #nir on red
-im.plotRGB(settembre, 2, 1, 3) #nir on green
-im.plotRGB(settembre, 2, 3, 1) #nir on blue
-
-#Calculating the DVI (Difference Vegetation Index)
-#band 1 = nir
-#band 2 = red
-dviluglio = luglio[[1]] - luglio[[2]] 
-plot(dviluglio, col = vir)
-
-dvisettembre = settembre[[1]] - settembre[[2]] 
-plot(dvisettembre, col = vir)
-
-par(mfrow = c(1,2))
-plot(dviluglio, col = vir)
-plot(dvisettembre, col = vir)
-
-#NDVI
-ndviluglio = dviluglio/(luglio[[1]] + luglio[[2]])
-ndvisettembre = dvisettembre/(settembre[[1]] + settembre[[2]])
-par(mfrow = c(1,2))
-plot(ndviluglio, col = vir)
-plot(ndvisettembre, col = vir)
-
-#classification: #quantiffying fire cover 
-julyc <- im.classify(luglio, num_cluster = 3)
-#class 1 = forest #questi cambiano ogni volta quindi da rivedere 
-#class 2 = human and fire modification
-#class 3 = sea and desert
-septc <- im.classify(settembre, num_cluster = 3)
-#class 1 = forest
-#class 2 = human and fire modification
-#class 3 = sea and desert
+#now we try yo do a classification on the dNBR to calculate the area damaged
+damaged_area <- im.classify(dNBR2, num_cluster = 3)
+#1 is no damaged
+#2 is damaged a bit
+#3 is extremly damaged
 
 #calculating frequencies
-freqluglio <- freq(lclassy) #the response of R will be the count of pixels organized in the two classes
-#proportions
-totluglio <- ncell(lclassy) #this is the total number of pixels
-propluglio = freqluglio / totluglio #is a proportion between the frequences and the total
-#percentages
+freqdNBR2 <- freq(damaged_area) #the response of R will be the count of pixels organized in the two classes
+freqdNBR2
+#1 = 1050714
+#2 = 535256
+#3 = 195238
+
+#calculating proportions
+totdNBR2 <- ncell(dNBR2) #this is the total number of pixels
+totdNBR2 #1781208
+propdNBR2 = freqdNBR2 / totdNBR2 #is a proportion between the frequencies and the total
+propdNBR2
+
+#calculatingpercentages
+percdNBR2 = propdNBR2 * 100 
+percdNBR2
+#1 = 59%
+#2 = 30%
+#3 = 11%
+
+###
+plot(NBR_julyB, col = viridis) # we use viridis to enhance differences
+plot(NBR_septB, col = viridis)
+
+NBRluglio_c <- im.classify(NBR_julyB, num_clusters = 2)
+NBRsett_c <- im.classify(NBR_septB, num_clusters = 2)
+#1 no vegetation, means fire
+#2 is vegetation
+
+##calculating frequencies, proportions and percentages for july
+freqluglio <- freq(NBRluglio_c) #the response of R will be the count of pixels organized in the two classes
+freqluglio
+#1 = 853151
+#2 = 928057
+
+totluglio <- ncell(NBR_julyB) #this is the total number of pixels
+totluglio #1781208
+propluglio = freqluglio / totluglio #is a proportion between the frequencies and the total
+propluglio
+
 percluglio = propluglio * 100 
+percluglio
+#1 = 47%
+#2 = 52%
+
+##calculating frequencies, proportions and percentages for september
+freqsett <- freq(NBRsett_c) #the response of R will be the count of pixels organized in the two classes
+freqsett
+#1 = 1148406
+#2 = 632802
+
+totsett <- ncell(NBR_septB) #this is the total number of pixels
+totsett #1781208
+propsett = freqsett / totsett #is a proportion between the frequencies and the total
+propsett
+
+percsett = propsett * 100 
+percsett
+#1 = 64%
+#2 = 36%
+
+#so there is an increasing in level 1 of 17% that is the cover land damaged by the fire in this area
+#we buid a dataframe
+#building the dataframe
+class <- c("vegetation", "desert soil and urbanization") #we mention the two classes that will appear on x asses 
+pjuly <- c(52, 47) #here the percentage of 1992
+pseptember <- c(36, 64) #here the percentage of 2006
+
+tabout <- data.frame(class, pjuly, pseptember) #we create a dataframe where we put the 2 classes and the related percentages for both years 
+tabout #we visualize the table
+
+#ggplot2 graphs
+g1 <- ggplot(tabout, aes(x = class, y = pjuly, color = class)) + geom_bar(stat = "identity",   fill = "white") + ylim(c(0, 100))
+#ggplot #with the initial part we only added a piece of the function: the table to plot, aestethic refers to the graphic structure so what we want in x and y and with which color; to create the graphic we need geom_bar
+#geom_bar #stat refers to the statistic type we want to represent: mean, median...we want the exact values so we put "identify"; fill refers to the filling color 
 
 #we do the same for september
-freqsettembre <- freq(sclassy) #the response of R will be the count of pixels organized in the two classes
-#proportions
-totsettembre <- ncell(sclassy) #this is the total number of pixels
-propsettembre = freqsettembre / totsettembre #is a proportion between the frequences and the total
-#percentages
-percsettembre = propsettembre * 100 
+g2 <- ggplot(tabout, aes(x = class, y = pseptember, color = class)) + geom_bar(stat = "identity", fill = "white") + ylim(c(0, 100))
 
+g1 + g2 #with this simple sum we join them using package patchwork
 
-#MANCA LA PARTE DEI PLOT MA FALLO LA VOLTA DEFINITIVA DATO CHE OGNI VOLTA TI CAMBIA LE CLASSI
-
+g1/g2 
